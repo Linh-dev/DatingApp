d34d8079e23aede87bc45ca6104d1e2ddd0e5ebf
LinhLD <linhld@dft.vn>
2021-11-22 22:56:50 +0700

    Update FE member - photo gallery and detail

--- API/DTOs/MemberDto.cs
+++ API/DTOs/MemberDto.cs
@@ -9,7 +9,6 @@ namespace API.DTOs
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-
         public string PhotoUrl { get; set; }
         public int Age { get; set; }
         public string KnownAs { get; set; }
